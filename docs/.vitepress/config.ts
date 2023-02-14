db325bb76dd9552a9e608f502567e9cd052d1ddf
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-02-15 00:05:04 +0800

    chore: update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,12 +10,12 @@ export default defineConfigWithTheme({
             {
                 text: '规约',
                 items: [
-                    {text: '关于手册', link: '/规约/README'},
-                    {text: '设计规约', link: '/规约/design-specification'},
-                    {text: '编程规约', link: '/规约/programming-specification'},
-                    {text: '单元测试', link: '/规约/unit-test'},
-                    {text: '安全规约', link: '/规约/security-specification'},
-                    {text: 'MySQL 数据库', link: '/规约/mysql-database'}
+                    {text: '关于手册', link: '/stipulations/README'},
+                    {text: '设计规约', link: '/stipulations/design-specification'},
+                    {text: '编程风格规约', link: '/stipulations/programming-specification'},
+                    {text: '单元测试', link: '/stipulations/unit-test'},
+                    {text: '安全规约', link: '/stipulations/security-specification'},
+                    {text: 'MySQL 数据库', link: '/stipulations/mysql-database'}
                 ]
             }
         ]
